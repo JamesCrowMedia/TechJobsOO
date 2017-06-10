daf376983259a0599d6275cbf155bb400255d34a
James Crow <jamescrowvideo@gmail.com>
2017-06-10 13:10:00 -0500

    Finished Assignment

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -19,7 +19,6 @@ namespace TechJobs.Controllers
         // The detail display for a given Job at URLs like /Job?id=17
         public IActionResult Index(int id)
         {
-
             Job singleJob = jobData.Find(id);
             return View(singleJob);
         }
@@ -33,10 +32,19 @@ namespace TechJobs.Controllers
         [HttpPost]
         public IActionResult New(NewJobViewModel newJobViewModel)
         {
-            // TODO #6 - Validate the ViewModel and if valid, create a 
-            // new Job and add it to the JobData data store. Then
-            // redirect to the Job detail (Index) action/view for the new Job.
-
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job
+                {
+                    Name           = newJobViewModel.Name,
+                    Employer       = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    Location       = jobData.Locations.Find(newJobViewModel.LocationID),
+                    CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID),
+                    PositionType   = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID)
+                };
+                jobData.Jobs.Add(newJob);
+                return Redirect("/Job?id=" + newJob.ID);
+            }
             return View(newJobViewModel);
         }
     }
